b614f366fa5d52afaf41073128ea68d10757ea26
Mark <mark@bhalash.com>
2014-10-17 16:47:19 +0100

    Friday evening work

--- 404.php
+++ 404.php
@@ -1,21 +1,16 @@
 <?php get_header(); ?>
 <div id="main" role="main">
-      
-    <div id="content">
- 
+    <div id="content"> 
           <div class="post">
             <div class="entry">
-                <?php if ( have_posts() )     the_post(); ?>
-                
-                <h1><?php _e('Aistear amú – tada anseo', 'wpzoom'); ?></h1>
-                
+                <?php if (have_posts()) the_post(); ?>
+                <h1><?php _e('Aistear amú – tada anseo', 'wpzoom'); ?></h1> 
                 <h3><?php _e('Ní bhfuarthas an leathanach atá á lorg agat.', 'wpzoom');?> </h3>
             </div><!-- / .entry -->
         </div><!-- /.post -->
-
     </div><!-- /#content -->
  
-     <?php get_sidebar(); ?>
+    <?php get_sidebar(); ?>
                
 </div> <!-- /#main -->
 <?php get_footer(); ?>
