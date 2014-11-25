65b30fe8af8bf77dfb323e9cdb0748a81cee2ef3
Mark <mark@bhalash.com>
2014-11-25 16:52:24 +0000

    Fixed problem where WordPress would escape characters incorrectly

--- mshare.php
+++ mshare.php
@@ -1,9 +1,10 @@
+<?php global $post; ?>
+
 <ul class="mshare">
     <?php $share = array(
         'blog'  => urlencode(get_bloginfo('name')),
         'url'   => urlencode(get_the_permalink()),
-        'title' => rawurlencode(get_the_title()),
-        'tuser' => urlencode('tuairiscnuacht')
+        'title' => urlencode($post->post_title),
     ); ?> 
 
     <li>
@@ -11,42 +12,42 @@
         href="mailto:?subject=<?php echo $share['title']; ?>&amp;body=<?php echo $share['url']; ?>" 
         data-rel="0" 
         target="_blank"  
-        title="Email '<?php the_title(); ?>'"></a>
+        title="Email <?php the_title_attribute(); ?>"></a>
     </li>
     <li>
         <a class="facebook" 
         href="//facebook.com/sharer.php?u=<?php echo $share['url'] ?>" 
         data-rel="1" 
         target="_blank" 
-        title="Share '<?php the_title(); ?>' on Facebook"></a>
+        title="Share <?php the_title_attribute(); ?> on Facebook"></a>
     </li>
     <li>
         <a class="google"
         href="//plus.google.com/share?url=<?php echo $share['url']; ?>" 
         data-rel="2" 
         target="_blank"
-        title="+1 '<?php the_title(); ?>"></a>
+        title="+1 <?php the_title_attribute(); ?>"></a>
     </li>
     <li>
         <a class="twitter"
         href="//twitter.com/share?via=<?php echo $share['tuser']; ?>&text=<?php echo $share['title']; ?>&url=<?php echo $share['url']; ?>"
         data-rel="3"
         target="_blank"
-        title="Tweet about '<?php the_title(); ?>'"></a>
+        title="Tweet about <?php the_title_attribute(); ?>"></a>
     </li>
     <li>
         <a class="reddit" 
         href="//reddit.com/submit?url=<?php echo $share['url']; ?>&title=<?php echo $share['title']; ?>" 
         data-rel="5"
         target="_blank"
-        title="Upvote '<?php the_title(); ?>' on Reddit"></a>
+        title="Upvote <?php the_title_attribute(); ?> on Reddit"></a>
     </li>
     <?php if (is_singular('post') && comments_open()) : ?>
         <li>
             <a class="comments"
             href="#comments" 
             data-rel="-1" 
-            title="Read comments on <?php echo $post->post_title; ?>"></a>
+            title="Read comments on <?php the_title_attribute(); ?>"></a>
         </li>
     <?php endif; ?>
 </ul>
