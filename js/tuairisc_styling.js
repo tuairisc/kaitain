985cf62562403a0ce3ed221abc4f2f88a7027e30
Mark <mark@bhalash.com>
2014-12-03 02:18:44 +0000

    Moved mobile menu toggle as part of menu refactor

--- js/tuairisc_styling.js
+++ js/tuairisc_styling.js
@@ -1,17 +1,65 @@
-'use strict'
+'use strict';
 
-function resizeFpt() {
-    // CSS for this box is a Byzantine mess that I could quite simply not untangle.
-    // The 'nuclear' option is this.
-    var fpt = '.feature-post-text';
-    var w = jQuery(fpt).parent().width() - jQuery(fpt).parent().find('img').first().outerWidth() - 15;
-    jQuery(fpt).css('width', w);
-}
+var responsiveBreak = 768;
 
-jQuery(function() {
-    resizeFpt();
-});
+jQuery(function($) {
+    function resizeFeaturePost() {
+        // CSS for this box is a Byzantine mess that I could quite simply not 
+        // untangle. The 'nuclear' option is this.
+        var fpt = '.feature-post-text';
+        var w = $(fpt).parent().width() - $(fpt).parent().find('img').first().outerWidth() - 15;
+        $(fpt).css('width', w);
+    }
 
-jQuery(window).resize(function() {
-    resizeFpt();
+    function mobileAdjust() {
+        var wrap = '.menu-wrap';
+
+        if(typeof window.orientation === 'undefined') {
+            $(wrap).removeAttr('style');
+         }
+
+        if($(window).width() <= responsiveBreak) {
+            $(wrap).addClass('mobile-menu');
+        }
+    }
+
+    function scrollMenuToggle() {
+        // Hide the menu until the window has scrolled past the first advert. 
+        // TODO: This stands to be refined after a consultation with Ciaran.
+        var menu = '#menu';
+
+        if ($(window).width() < responsiveBreak && $(window).scrollTop() < $('.g-1').outerHeight() + 20) {
+            $(menu).css({
+                'top': $('.g-1').scrollHeight + 50,
+                'display' : 'initial'
+            });
+        } else {
+            // $(menu).css('top', 20);
+        }
+    }
+
+    $(window).scroll(function() {
+        // scrollMenuToggle();
+    });
+
+    // scrollMenuToggle();
+    mobileAdjust();
+    resizeFeaturePost();
+
+    $('#toggle-main').click(function() {
+        $('.menu-wrap').toggle();
+        $('.mobile-menu').toggleClass("active"); 
+        return false;
+    });
+
+    $(window).resize(function() {
+        mobileAdjust();
+        resizeFeaturePost();
+    });
+
+    // By default #secondmenu would display on mobile devices until this 
+    // function was called to hide its parent. It looked unsightly and 
+    // reduced the ability of the user to immediately navigate the site. 
+    // I changed its CSS to hide it until the function is called. 
+    $('#secondmenu').show();
 });
