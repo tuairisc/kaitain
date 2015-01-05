4790c772b897a27fcb4ef0e28ed70a62419e78a8
Mark <mark@bhalash.com>
2015-01-05 13:58:59 +0000

    Removed mentions of WPZOOM
    The code is all-new.

--- functions/user/tuairisc-mostviewed.php
+++ functions/user/tuairisc-mostviewed.php
@@ -1,20 +1,15 @@
 <?php class tuairisc_popular extends WP_Widget {
     /* tuairisc_popular Widget
      * -----------------------
-     * This widget is a re-write of WPZOOM's popular post widget. Both the form 
-     * and widget functions were re-written from almost the ground up; there's 
-     * virtually no remaining original code. 
-     * 
      * This widget uses the tuairisc_view_counter meta key of posts. The view 
      * counter was a requested way to quickly and non-canonically determine the
-     * most popular posts on the site. The stock WPZOOM widget directly accessed
+     * most popular posts on the site. The stock widget directly accessed
      * the database, and a good deal of the code was tied to parsing this. 
-     * I instead use WP_Query to fetch posts.  
      */ 
     public function __construct() {
         parent::__construct(
             'tuairisc_popular',
-            'Tuairisc Popular',
+            'Tuairisc\'s Popular Posts',
             array(
                 'description' => 'An ordered list of popular Tuairisc posts sorted by view count.',
             )
@@ -24,19 +19,19 @@
     public function widget($args, $instance) {
         extract($args);
 
-        $title = apply_filters('widget_title', $instance['title']);
+        $title = apply_filters('widget_title', $instance['widget_title']);
         $counter_meta_key = 'tuairisc_view_counter';
 
-        // Get current date and subtract $instance['sincewhen'] days from it. 
+        // Get current date and subtract $instance['elapsed_days'] days from it. 
         $end_date = new DateTime();
         $start_date = new DateTime();
-        $start_date = $start_date->sub(new DateInterval('P' . $instance['sincewhen'] . 'D'));
+        $start_date = $start_date->sub(new DateInterval('P' . $instance['elapsed_days'] . 'D'));
 
         $popular_query = new WP_Query(array(
             'cat' => -184,
             'post_type' => 'post',
             'meta_key' => $counter_meta_key, 
-            'posts_per_page' => $instance['maxposts'],
+            'posts_per_page' => $instance['max_posts'],
             'orderby' => $counter_meta_key,
             'order' => 'DESC',
             'date_query' => array(
@@ -59,7 +54,7 @@
         if (!empty($args['before_widget'])) {
             // Widget container and title.
             echo $before_widget;
-            echo $before_title . apply_filters('widget_title', $instance['title']) . $after_title;
+            echo $before_title . apply_filters('widget_title', $instance['widget_title']) . $after_title;
         }
 
         echo '<ul class="tuairisc-popular-list">';
@@ -94,17 +89,17 @@
 
     function update($new_instance, $old_instance) {
         $instance = array();
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['maxposts'] = $new_instance['maxposts'];
-        $instance['sincewhen'] = $new_instance['sincewhen'];
+        $instance['widget_title'] = strip_tags($new_instance['widget_title']);
+        $instance['max_posts'] = $new_instance['max_posts'];
+        $instance['elapsed_days'] = $new_instance['elapsed_days'];
         return $instance;
     }
 
     public function form($instance) {
         $defaults = array(
-            'title' => 'Most Viewed',
-            'maxposts' => 10,
-            'sincewhen' => '7'
+            'widget_title' => 'Most Viewed',
+            'max_posts' => 10,
+            'elapsed_days' => '7'
         );
 
         $instance = wp_parse_args($instance, $defaults);
@@ -115,20 +110,20 @@
                  * default selected options for both select values. This proved 
                  * difficult for me to pick apart, parse and edit. */
                 jQuery(function($) {
-                    // Set 'sincewhen' selected option.
-                    $('<?php echo "#" . $this->get_field_id("sincewhen"); ?>').val('<?php echo $instance["sincewhen"]; ?>');
-                    // Set 'maxposts' selected option. 
-                    $('<?php echo "#" . $this->get_field_id("maxposts"); ?>').val('<?php echo $instance["maxposts"]; ?>');
+                    // Set 'elapsed_days' selected option.
+                    $('<?php echo "#" . $this->get_field_id("elapsed_days"); ?>').val('<?php echo $instance["elapsed_days"]; ?>');
+                    // Set 'max_posts' selected option. 
+                    $('<?php echo "#" . $this->get_field_id("max_posts"); ?>').val('<?php echo $instance["max_posts"]; ?>');
                 });
             </script>
 
             <p>
-                <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wpzoom'); ?></label><br />
-                <input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" type="text"  class="widefat" />
+                <label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Title:', 'wpzoom'); ?></label><br />
+                <input id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" value="<?php echo $instance['widget_title']; ?>" type="text"  class="widefat" />
             </p>
             <p>
-                <label for="<?php echo $this->get_field_id('sincewhen'); ?>"><?php _e('Since:', 'wpzoom'); ?></label><br />
-                <select id="<?php echo $this->get_field_id('sincewhen'); ?>" name="<?php echo $this->get_field_name('sincewhen'); ?>">
+                <label for="<?php echo $this->get_field_id('elapsed_days'); ?>"><?php _e('Since:', 'wpzoom'); ?></label><br />
+                <select id="<?php echo $this->get_field_id('elapsed_days'); ?>" name="<?php echo $this->get_field_name('elapsed_days'); ?>">
                     <option value="36500">All Time</option>
                     <option value="365">This Year</option>
                     <option value="28">This Month</option>
@@ -137,8 +132,8 @@
                 </select>
             </p>
             <p>
-                <label for="<?php echo $this->get_field_id('maxposts'); ?>"><?php _e('Posts To Display:', 'wpzoom'); ?></label><br />
-                <select id="<?php echo $this->get_field_id('maxposts'); ?>" name="<?php echo $this->get_field_name('maxposts'); ?>">
+                <label for="<?php echo $this->get_field_id('max_posts'); ?>"><?php _e('Posts To Display:', 'wpzoom'); ?></label><br />
+                <select id="<?php echo $this->get_field_id('max_posts'); ?>" name="<?php echo $this->get_field_name('max_posts'); ?>">
                     <?php for ($i = 1; $i <= 10; $i++) : ?>
                         <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
                     <?php endfor; ?>
