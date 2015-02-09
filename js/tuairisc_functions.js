8da770d286444f9fba40dc0388da7b12a0ecfc1b
Mark <mark@bhalash.com>
2015-02-09 13:48:06 +0000

    Renamed file and removed unused JS

--- js/tuairisc_functions.js
+++ js/tuairisc_functions.js
@@ -1,18 +1,10 @@
 'use strict';
 
 jQuery(function($) {
-    function resizeFeaturePost() {
-        // CSS for this box was a Byzantine mess. I wasn't able to consistently
-        // set its style with CSS alone. 
-        var fpt = '.feature-post-text';
-        var w = $(fpt).parent().width() - $(fpt).parent().find('img').first().outerWidth() - 15;
-        $(fpt).css('width', w);
-    }
-
-    resizeFeaturePost();
-
-    $(window).resize(function() {
-        resizeFeaturePost();
+    $('#menu a').each(function() {
+        if ($(this).siblings('ul').length > 0) {
+            $(this).addClass('submenu-indicator');
+        }
     });
 
     if (Modernizr.touch) {
