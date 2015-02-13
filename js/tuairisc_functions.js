c2e668f89ad6bfbd1f453d9f084c305aaefec19f
Mark <mark@bhalash.com>
2015-02-13 02:04:28 +0000

    Merged JavaScript files

--- js/tuairisc_functions.js
+++ js/tuairisc_functions.js
@@ -1,6 +1,11 @@
 'use strict';
 
 jQuery(function($) {
+    /*
+     * Site Navigation Menu
+     * --------------------
+     */
+
     $('#menu a').each(function() {
         if ($(this).siblings('ul').length > 0) {
             $(this).addClass('submenu-indicator');
@@ -23,4 +28,48 @@ jQuery(function($) {
             $(this).find('a').removeClass('firstclick');
         });
     }
+
+    var sizes = [
+        // Email
+        '',
+        // Facebook
+        'width=450,height=257',
+        // Google
+        'width=500,height=300',
+        // Twitter
+        'width=470,height=260',
+        // Pinterest
+        'width=756,height=320',
+        // Reddit
+        '',
+        // Tumblr
+        'width=470,height=470',
+    ]; 
+
+    /*
+     * Social Sharing Popouts
+     * ----------------------
+     */
+
+    $('article .mshare a').click(function(click) {
+        // .mshare sharing links.
+        var rel = parseInt($(this).data('rel'));
+
+        if (sizes[rel] != '' && rel > 0) {
+            var href = $(this).attr('href');
+            var name = 'target="_blank';
+            var size = sizes[rel];
+
+            window.open(href, name, size);
+            click.preventDefault();
+        }
+    });
+
+    $('.newsletter a').click(function(click) {
+        // Signup for the Tuairisc mailing list.
+        var href = $(this).attr('href');
+        var name = 'target="_blank';
+        window.open(href,name,'width=400,height=630');
+        click.preventDefault();
+    });
 });
