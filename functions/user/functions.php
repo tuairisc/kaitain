43858438c30776090fe9215aceaa76ebb9aef99a
Mark <mark@bhalash.com>
2015-02-13 02:14:05 +0000

    Several changes
    * Split job code to its own file.
    * Separated social fallback into an array.
    * Ensure out-of-loop loading of excerpt in social functions.
    * Moved requireds to head of file.
    * Removed reference to delete JavaScript files.
    * Updated thumbnail size option to match Tuairisc content width.

--- functions/user/functions.php
+++ functions/user/functions.php
@@ -3,6 +3,13 @@
 /* You can add custom functions below, in the empty area
 =========================================================== */
 
+// Load custom Tuairisc widgets.
+$widget_path = get_template_directory() . '/functions/user/';
+require_once $widget_path . 'tuairisc-authors.php';
+require_once $widget_path . 'tuairisc-mostviewed.php';
+// Load jobs
+require_once $widget_path . 'tuairisc-jobs.php';
+
 /* 
  * # Category Name   Hex Colour  Category ID
  * -----------------------------------------
@@ -49,8 +56,6 @@ function tuairisc_scripts() {
     wp_enqueue_script('tuairisc-adrotate', get_stylesheet_directory_uri() . '/js/tuairisc_adrotate.js', array('jquery'), '1.0', true);
     // Some styling isn't handled correctly by CSS.
     wp_enqueue_script('tuairisc-functions', get_stylesheet_directory_uri() . '/js/tuairisc_functions.js', array('jquery'), '1.0', true);
-    // Sharing links popout. 
-    wp_enqueue_script('tuairisc-share-popout', get_stylesheet_directory_uri() . '/js/tuairisc_share_popout.js', array('jquery'), '1.0', true);
     // Modernizr, for menu touch events.
     wp_enqueue_script('tuairisc-modernizr', get_stylesheet_directory_uri() . '/js/tuairisc_modernizr_touch.js', array('jquery'), '1.0', true);
 }
@@ -92,7 +97,7 @@ function get_breadcrumb() {
         echo '<span>' . category_description() . '</span>';
     } else if (is_singular('foluntais')) {
         get_job_category_link($post_id, true, '&nbsp;');
-    } else if (is_foluntais()) {
+    } else if (is_job()) {
         get_job_category_link($post_id, false, '&nbsp;');
     }
 }
@@ -102,13 +107,13 @@ function is_greann() {
      * with other, more serious, segments of the website. */
 
     if (is_category()) {
-        $id = get_parent_id(get_query_var('cat'));
+        $cat_id = get_parent_id(get_query_var('cat'));
     } else if (is_single() && has_category()) {
-        $id = get_the_category();
-        $id = $cat[0]->cat_ID;
+        $cat_id = get_the_category();
+        $cat_id = $cat[0]->cat_ID;
     }
 
-    return ($id == 158) ? true : false;
+    return ($cat_id == 158) ? true : false;
 }
 
 function breadcrumbs_get_id() {
@@ -132,7 +137,7 @@ function breadcrumbs_get_id() {
         }
     } else if (is_category()) {
         $id = get_parent_id(get_query_var('cat'));
-    } else if (is_foluntais()) {
+    } else if (is_job()) {
         $id = 799;
     } else {
         return;
@@ -252,7 +257,7 @@ function has_local_avatar($user_id) {
         $user_id = get_the_author_meta('ID');
     }
 
-    return (strpos(get_avatar_url($user_id), 'gravatar') == false) ? true : false;
+    return (strpos(get_avatar_url($user_id), 'gravatar') == false);
 }
 
 function day_to_irish($day) {
@@ -293,7 +298,7 @@ function default_author($author_id = null) {
     }
 
     $default_author = 37;
-    return ($author_id == $default_author) ? true : false;
+    return ($author_id == $default_author);
 }
 
 function month_to_irish($month) {
@@ -452,7 +457,7 @@ function is_columnist_article() {
     $col_article = strtolower($col_article);
     $col_article = strip_tags($col_article);
 
-    return ($col_article === '1') ? true : false;
+    return ($col_article === '1');
 }
 
 function tweak_title($title, $sep) {
@@ -563,25 +568,31 @@ function social_meta() {
     twitter_meta();
 }
 
-$fallback_desc = 'Cuireann Tuairisc.ie seirbhís nuachta Gaeilge '
-    . 'ar fáil do phobal uile na Gaeilge, in Éirinn agus thar lear. Té sé '
-    . 'mar aidhm againn oibriú i gcónaí ar leas an phobail trí nuacht, '
-    . 'eolas, anailís agus siamsaíocht ar ardchaighdeán a bhailiú, a '
-    . 'fhoilsiú agus a chur sa chúrsaíocht.';
-$fallback_image = get_template_directory_uri() . '/images/tuairisc_fallback.jpg';
+$fallback = array(
+    'publisher' => 'https://www.facebook.com/tuairisc.ie',
+    'image' => get_template_directory_uri() . '/images/tuairisc_fallback.jpg',
+    'twitter' => '@tuairiscnuacht',
+    'description' => 'Cuireann Tuairisc.ie seirbhís nuachta Gaeilge '
+        . 'ar fáil do phobal uile na Gaeilge, in Éirinn agus thar lear. Té sé '
+        . 'mar aidhm againn oibriú i gcónaí ar leas an phobail trí nuacht, '
+        . 'eolas, anailís agus siamsaíocht ar ardchaighdeán a bhailiú, a '
+        . 'fhoilsiú agus a chur sa chúrsaíocht.',
+);
 
 function twitter_meta() {
     /* Social Meta Information for Twitter
      * ------------------------------------
      * This /should/ be all of the relevant information for Twitter. */
-    global $fallback_desc, $fallback_image, $post;
+    global $fallback, $post;
+    $the_post = get_post($post->ID);
+    setup_postdata($the_post);
 
     $site_meta = array(
         'twitter:card' => 'summary',
-        'twitter:site' => '@tuairiscnuacht',
+        'twitter:site' => $fallback['twitter'],
         'twitter:title' => get_the_title(),
-        'twitter:description' => (is_single()) ? get_the_excerpt() : $fallback_desc,
-        'twitter:image:src' => (is_single()) ? get_thumbnail_url() : $fallback_image,
+        'twitter:description' => (is_single()) ? get_the_excerpt() : $fallback['description'],
+        'twitter:image:src' => (is_single()) ? get_thumbnail_url() : $fallback['image'],
         'twitter:url' => get_site_url() . $_SERVER['REQUEST_URI'],
     );
 
@@ -594,15 +605,17 @@ function facebook_meta() {
     /* Social Meta Information for Facebook
      * ------------------------------------
      * This /should/ be all of the relevant information for Facebook. */
-    global $fallback_desc, $fallback_image, $post;
+    global $fallback, $post;
+    $the_post = get_post($post->ID);
+    setup_postdata($the_post);
     $thumb = get_thumbnail_url($post->ID, 'full', true);
 
     $site_meta = array(
         'og:title' => get_the_title(),
         'og:site_name' => get_bloginfo('name'),
         'og:url' => get_site_url() . $_SERVER['REQUEST_URI'],
-        'og:description' => (is_single()) ? get_the_excerpt() : $fallback_desc,
-        'og:image' => (is_single()) ? $thumb[0] : $fallback_image,
+        'og:description' => (is_single()) ? get_the_excerpt() : $fallback['description'],
+        'og:image' => (is_single()) ? $thumb[0] : $fallback['image'],
         'og:image:width' => $thumb[1],
         'og:image:height' => $thumb[2],
         'og:type' => (is_single()) ? 'article' : 'website',
@@ -611,11 +624,23 @@ function facebook_meta() {
 
     if (is_single()) {
         $category = get_the_category($post->ID);
+        $tags = get_the_tags();
+        $taglist = '';
+        $i = 0;
+
+        foreach ($tags as $the_tag) {
+            if ($i > 0) {
+                $taglist .= ', ';
+            }
+
+            $taglist .= $the_tag->name;
+            $i++;
+        }
 
         $article_meta = array(
             'article:section' => $category[0]->cat_name,
-            'article:tag' => get_the_tags(),
-            'article:publisher' => 'https://www.facebook.com/tuairisc.ie',
+            'article:publisher' => $fallback['publisher'],
+            'article:tag' => $taglist,
         );
 
         $site_meta = array_merge($site_meta, $article_meta);
@@ -626,267 +651,8 @@ function facebook_meta() {
     }
 }
 
-/*
- * Foluntais Custom Post Type
- * --------------------------
- * This custom post type is used to highlight and differentiate jobs across the 
- * Tuairisc website. 
-*/
-
-function register_job_type() {
-    /* Register the foluntais custom post type, used for job listsings across 
-     * the Tuairisc website. */
-
-    $labels = array(
-        'name'               => _x('Jobs', 'post type general name'),
-        'singlular_name'     => _x('Job', 'post type individual name'),
-        'add_new'            => _x('Add New', 'job'),
-        'add_new_item'       => __('Add New Job'),
-        'edit_item'          => __('Edit Job'),
-        'new_item'           => __('New Job'),
-        'menu_name'          => __('Jobs'),
-        'all_items'          => __('All Jobs'),
-        'search_items'       => __('Search Jobs'),
-        'not_found'          => __('No jobs found'),
-        'not_found_in_trash' => __('No jobs found in the Trash'),
-        'parent_item_colon'  => '',
-    );
-
-    $args = array(
-        'description'   => 'Tuairisc.ie job listings',
-        'has_archive'   => true,
-        'labels'        => $labels,
-        'menu_position' => 4,
-        'public'        => true,
-        'supports'      => array('title', 'editor', 'thumbnail', 'excerpt', 'custom-fields'),
-        'taxonomies'    => array('job_categories','job_tags'),
-    );
-
-    register_post_type('foluntais', $args);
-    flush_rewrite_rules();
-    remove_post_type_support('foluntais','comments');
-}
-
-function register_job_taxonomies() {
-    /* Create the custom taxonomy for jobs. It was considered inappropraite to
-     * include jobs in the common tags and categories, so therefore they have a 
-     * separate, parallel scheme. */
-
-    /* Job Categories
-     * -------------- */
-
-    $cat_labels = array(
-        'name'              => _x('Job Categories', 'taxonomy general name'),
-        'singular_name'     => _x('Job Category', 'taxonomy singular name'),
-        'search_items'      => __('Search Job Categories'),
-        'all_items'         => __('All Job Categories'),
-        'parent_item'       => __('Parent Cateory'),
-        'parent_item_colon' => __('Parent Category:'),
-        'edit_item'         => __('Edit Category'),
-        'update_item'       => __('Update Category'),
-        'add_new_item'      => __('Add Category'),
-        'new_item_name'     => __('New Category Name'),
-        'menu_name'         => __('Job Categories'),
-    );
-
-    $cat_args = array(
-        'hierarchical'      => true,
-        'labels'            => $cat_labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array('slug' => 'job-categories'),
-    );
-
-    /* Job Tags
-     * -------- */
-
-    $tag_labels = array(
-        'name'              => _x('Job Tags', 'taxonomy general name'),
-        'singular_name'     => _x('Job Tag', 'taxonomy singular name'),
-        'search_items'      => __('Search Tags'),
-        'all_items'         => __('All Tags'),
-        'edit_item'         => __('Edit Tags'),
-        'update_item'       => __('Update Tag'),
-        'add_new_item'      => __('Add New Tag'),
-        'new_item_name'     => __('New Tag'),
-        'menu_name'         => __('Job Tags'),
-    );
-
-    $tag_args = array(
-        'hierarchical'      => false,
-        'labels'            => $tag_labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array('slug' => 'job-tags'),
-    );
-
-    register_taxonomy('job_categories', array('foluntais'), $cat_args);
-    register_taxonomy('job_tags', array('foluntais'), $tag_args);
-}
-
-function job_messages($messages) {
-    /* Error and help messages related to the jobs custom post type. */
-
-    global $post, $post_ID;
-    
-    $messages['foluntais'] = array(
-        0  => '', 
-        1  => sprintf(__('Job updated. <a href="%s">View job</a>', 'wpzoom'), esc_url(get_permalink($post_ID))),
-        2  => __('Custom field updated.', 'wpzoom'),
-        3  => __('Custom field deleted.', 'wpzoom'),
-        4  => __('Job updated.'),
-        5  => isset($_GET['revision']) ? sprintf( __('Job restored to revision from %s', 'wpzoom'), wp_post_revision_title( (int) $_GET['revision'], false)) : false,
-        6  => sprintf(__('Job published. <a href="%s">View job</a>', 'wpzoom'), esc_url(get_permalink($post_ID))),
-        7  => __('Product saved.', 'wpzoom'),
-        8  => sprintf(__('Job submitted. <a target="_blank" href="%s">Preview job</a>', 'wpzoom'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
-        9  => sprintf(__('Job scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview job</a>', 'wpzoom'), date_i18n(__('M j, Y @ G:i',' wpzoom'), strtotime($post->post_date)), esc_url(get_permalink($post_ID))),
-        10 => sprintf(__('Job draft updated. <a target="_blank" href="%s">Preview job</a>','wpzoom'), esc_url(add_query_arg('preview','true', get_permalink($post_ID) ) ) ),
-    );
-
-    return $messages;
-}
-
-function is_foluntais($post_id = null) {
-    /* Is the post type explicitly of the foluntais type? */
-
-    $type = 'foluntais';
-
-    if ('' == $post_id) {
-        $post_id == get_the_ID();
-    }
-
-    return (get_post_type($post_id) == $type) ? true : false;
-}
-
-function has_job_category($term = null, $post_id = null) {
-    /* Test whether a post has any given term out of a given taxonomy. This
-     * function parallels has_category() for the job_categories taxonomy. */
-
-    $taxonomy = 'job_categories';
-
-    if ('' == $post_id) {
-        $post_id = get_the_ID();
-    }
-
-    if ('' == $term) {
-        $term_list = get_terms($taxonomy);
-
-        foreach ($term_list as $a) {
-            array_push($term, $a->term_id);
-        }
-    }
-
-    return (has_term($term, $taxonomy, $post_id)) ? true : false;
-}
-
-function job_category_id($post_id) {
-    /* Shorthand function to return only the ID of a given term in the custom
-     * taxonomy for job categories. */
-
-    $taxonomy = 'job_categories';
-
-    if ('' == $post_id) {
-        $post_id = get_the_ID();
-    }
-
-    if (!has_job_category()) {
-        return -1;
-    }
-
-    $cat_id = wp_get_post_terms($post_id, $taxonomy, array('fields' => 'ids'));
-    return $cat_id[0];
-}
-
-function job_category_name($post_id = null) {
-    /* Shorthand function to return only the name of a given term in the custom
-     * taxonomy for job categories. */
-
-    if ('' == $post_id) {
-        $post_id = get_the_ID();
-    }
-
-    if (!has_job_category()) {
-        return -1;
-    }
-
-    $term = get_term(job_category_id($post_id), 'job_categories');
-    return $term->name;
-}
-
-function job_category_link($post_id) {
-    /* Return the link to the given job post type. */
-
-    if ('' == $post_id) {
-        $post_id = get_the_ID();
-    }
-
-    $term = get_term(job_category_id($post_id), 'job_categories');    
-    return get_term_link($term);
-}
- 
-function get_job_category_link($post_id = null, $use_child = false, $sep = '/') {
-    /* This functions mirrors the function of get_category_parents(), for job
-     * postings. This function needs to be refactored. */
-
-    if ('' == $post_id) {
-        $post_id == get_the_ID();
-    }
-
-    if (!has_job_category()) {
-        return;
-    }
-
-    $parent_link   = get_post_type_archive_link('foluntais'); 
-    $parent_name   = get_post_type($id);
-    $anchor = '<a href="' . $parent_link . '">' . $parent_name . '</a>' . $sep;
-
-    if (true == $use_child) {
-        $child_name = job_category_name($post_id);
-        $child_link = job_category_link($post_id);
-        $anchor .= '<a href="' . $child_link . '">'. $child_name . '</a>';
-    }
-
-    echo $anchor;
-}
-
-function job_category_color($post_id = null) {
-    /* Used on the archive page for job categories. If the post has an assigned 
-     * colour, show the colour.*/
-
-    global $banner_colors;
-
-    if ('' == $post_id) {
-        $post_id = get_the_ID();
-    }
-
-    if (!has_job_category()) {
-        // Hide the box if there's no valid category.
-        echo 'display: none;';
-        return;
-    }
-
-    $cat_id = job_category_id($post_id);
-    $bg_open = 'background-color:';
-    $bg_close = ';';
-
-    if (array_key_exists($cat_id, $banner_colors)) {
-        echo $bg_open . $banner_colors[$cat_id] . $bg_close;
-    } else {
-        echo $bg_open . $banner_colors[799] . $bg_close;
-    }
-}
-
-// Load custom Tuairisc widgets.
-$widget_path = get_template_directory() . '/functions/user/';
-require_once $widget_path . 'tuairisc-authors.php';
-require_once $widget_path . 'tuairisc-mostviewed.php';
-
-// Add custom post type for job listings.
-add_action('init', 'register_job_type');
-add_action('init', 'register_job_taxonomies', 0);
-add_filter('post_updated_messages', 'job_messages');
+// Change large size to match post content width.
+update_option('large_size_w', 770);
 // Load JavaScript scripts. 
 add_action('wp_enqueue_scripts', 'tuairisc_scripts');
 add_action('wp_enqueue_scripts', 'tuairisc_styles');
