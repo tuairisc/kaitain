cabb39cdfb1fc71b305abf1f289460592374c911
Mark <mark@bhalash.com>
2015-03-29 20:11:58 +0100

    Changed mixin to reduce rule spam

--- tuairisc-featured.php
+++ tuairisc-featured.php
@@ -7,41 +7,31 @@ $query = new WP_Query(array(
     'meta_value' => 1
 ));
 
-$class = array(
-    'parent' => 'tuairisc-featured',
-    'row' => 'tuairisc-featured-row',
-    'block' => 'tuairisc-featured-block'
-);
-
 if ($query->have_posts()) {
-    printf('<div class="%s">', $class['parent']);
+    printf('<div class="tuairisc-featured">');
 
     while ($query->have_posts()) {
         $query->the_post();
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
         if ($query->current_post == 0 && $paged == 1) {
+
             // Call lead article.
             get_template_part('article', 'lead');
+
         } else {
-            if ($query->current_post % 4 == 1) {
-                printf('<div class="%s">', $class['row']);
-            }
-            
-            if ($query->current_post % 2 == 1) {
-                printf('<div class="%s">', $class['block']);
+
+            if ($query->current_post == 1) {
+                printf('<div class="tuairisc-featured-row">');
             }
 
             // Call secondary article.
             get_template_part('article', 'featured');
 
-            if ($query->current_post % 2 == 0) {
+            if ($query->current_post == 8) {
                 printf('</div>');
             }
 
-            if ($query->current_post % 4 == 0) {
-                printf('</div>');
-            }
         }
     }
 
