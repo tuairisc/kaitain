33645459372615f06947e5cdc7b1a8e9af8993e1
Mark <mark@bhalash.com>
2015-04-14 21:37:14 +0100

    Added comments to header of file

--- tuairisc-featured.php
+++ tuairisc-featured.php
@@ -1,4 +1,20 @@
 <?php
+
+/**
+ * Home Page Top Featured Posts
+ * ----------------------------
+ * This script generates the code for the top/featured articles on the Tuairisc
+ * home page.
+ * 
+ * @category   WordPress File
+ * @package    Tuairisc.ie Gazeti Theme
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    1.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ */
+
 $query = new WP_Query(array(
     'post__not_in' => get_option('sticky_posts'),
     'posts_per_page' => 9,
@@ -15,7 +31,6 @@ if ($query->have_posts()) {
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
         if ($query->current_post == 0 && $paged == 1) {
-
             // Call lead article.
             get_template_part('article', 'lead');
 
