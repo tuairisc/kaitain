d6bdcaffa99181468cd1b6d7ffba944670c53103
Mark <mark@bhalash.com>
2015-04-14 22:29:09 +0100

    Reordered items again

--- mshare.php
+++ mshare.php
@@ -9,6 +9,33 @@
     ); ?> 
 
     <li>
+        <a class="print" 
+        href="javascript:window.print()" 
+        data-rel="-1" 
+        title="Print <?php the_title_attribute(); ?>"></a>
+    </li>
+    <li>
+        <a class="facebook" 
+        href="//facebook.com/sharer.php?u=<?php echo $share['url'] ?>" 
+        data-rel="1" 
+        target="_blank" 
+        title="Share <?php the_title_attribute(); ?> on Facebook"></a>
+    </li>
+    <li>
+        <a class="twitter"
+        href="//twitter.com/share?via=<?php echo $share['tuser']; ?>&text=<?php echo $share['title']; ?>&url=<?php echo $share['url']; ?>&related=@<?php echo $share['tuser']; ?>"
+        data-rel="3"
+        target="_blank"
+        title="Tweet about <?php the_title_attribute(); ?>"></a>
+    </li>
+    <li>
+        <a class="google"
+        href="//plus.google.com/share?url=<?php echo $share['url']; ?>" 
+        data-rel="2" 
+        target="_blank"
+        title="+1 <?php the_title_attribute(); ?>"></a>
+    </li>
+    <li>
         <a class="email" 
         href="mailto:?subject=<?php echo $share['title']; ?>&amp;body=<?php echo $share['url']; ?>" 
         data-rel="0" 
@@ -21,33 +48,6 @@
         data-rel="5"
         target="_blank"
         title="Upvote <?php the_title_attribute(); ?> on Reddit"></a>
-    </li>
-    <li>
-        <a class="google"
-        href="//plus.google.com/share?url=<?php echo $share['url']; ?>" 
-        data-rel="2" 
-        target="_blank"
-        title="+1 <?php the_title_attribute(); ?>"></a>
-    </li>
-    <li>
-        <a class="twitter"
-        href="//twitter.com/share?via=<?php echo $share['tuser']; ?>&text=<?php echo $share['title']; ?>&url=<?php echo $share['url']; ?>&related=@<?php echo $share['tuser']; ?>"
-        data-rel="3"
-        target="_blank"
-        title="Tweet about <?php the_title_attribute(); ?>"></a>
-    </li>
-    <li>
-        <a class="facebook" 
-        href="//facebook.com/sharer.php?u=<?php echo $share['url'] ?>" 
-        data-rel="1" 
-        target="_blank" 
-        title="Share <?php the_title_attribute(); ?> on Facebook"></a>
-    </li>
-    <li>
-        <a class="print" 
-        href="javascript:window.print()" 
-        data-rel="-1" 
-        title="Print <?php the_title_attribute(); ?>"></a>
     </li>
     <?php if (is_singular('post') && comments_open()) : ?>
         <li>
