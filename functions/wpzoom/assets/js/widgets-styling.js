638a844c7acd530dafa22033b95ea992da73f2ed
Mark <mark@bhalash.com>
2015-04-20 12:34:32 +0100

    Rewrote, added boilerplate and minified

--- functions/wpzoom/assets/js/widgets-styling.js
+++ functions/wpzoom/assets/js/widgets-styling.js
@@ -1,11 +1,17 @@
-jQuery(document).ready(function($) {
-    var wpzoom_widget_regexp = /wpzoom/;
-    $('.widget').each(function(i, el) {
-        var el = $(el);
-        var id = el.prop('id');
+/**
+ * Add Widget Styling Class
+ * ------------------------
+ * Add extra class to theme widgets for styling purposes.
+ * 
+ * @category   WordPress File
+ * @package    Tuairisc.ie Theme
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  2014-2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    2.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ */
 
-        if (wpzoom_widget_regexp.test(id)) {
-            $(el).addClass('wpz_widget_style');
-        }
-    });
-});
+jQuery(function(e){var t={regex:/wpzoom/,type:".widget",newType:"wpz_widget_style"}
+e(t.type).each(function(p,n){var r=e(n),w=r.prop("id")
+t.regex.text(w)&&r.addClass(t.newType)})})
