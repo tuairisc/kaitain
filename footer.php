af07ccc6609da8b70528a0feb04bd8d500eb9eff
Mark <mark@bhalash.com>
2015-04-20 16:09:46 +0100

    Changed class to ID

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <?php if (is_active_sidebar('footer-full')) : ?>
-            <div class="widgets">
+            <div id="widgets">
                 <?php dynamic_sidebar('Footer (full-width)'); ?>
             </div>
         <?php endif; ?>
