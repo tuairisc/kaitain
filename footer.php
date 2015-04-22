a4f756f02cb9968ff5ae07c61aa6b82ebc8124fb
Mark <mark@bhalash.com>
2015-04-22 20:03:16 +0100

    Added boilerplate

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+<?
+/**
+ * Site Footer
+ * -----------
+ * @category   PHP Script
+ * @package    Tuairisc.ie Theme
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2014-2015, Tuairisc Bheo Teo
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    2.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ */
+?>
+
             </div> <?php // End #main-interior ?>
         <div class="section-trim trim-bottom"></div>
     </div> <?php // End #main ?>
