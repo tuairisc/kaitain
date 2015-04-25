e01b25b0d2d284345d5a7588118ff470369a501f
Mark <mark@bhalash.com>
2015-04-25 23:59:39 +0100

    Expanded on section
    1. Added delineation between segments.
    2. Added array of intended sections-categories.
    3. Added test to determine loop type.
    4. Added function to get ID of ultimate category parent.
    5. Added test function for debugging.

--- functions/sections.php
+++ functions/sections.php
@@ -11,44 +11,137 @@
  * @link       https://github.com/bhalash/tuairisc.ie
  */ 
 
-// 1. Define a section
-// 2. Associate an object-post, category, tag, type or page-with a section.
+/**
+ * Variables
+ * -----------------------------------------------------------------------------
+ */
 
-class Section {
-    function add_filter($action, $function, $priority = 10, $accepted_args = 1) {
-        add_filter($action, $function, $priority, $accepted_args);
-    }
+$body_classes = array(
+    'has_section' => 'is-site-section',
+    'individual_section' => 'site-section-'
+);
 
-    public function add_section_body_class($classes) {
-        if (is_single()) {
-            $classes[] = 'yolo-swag-lol';
+$section_categories = array(
+    'tuairimiocht' => 154,
+    'spoirt' => 155,
+    'cultur' => 156, 
+    'saol' => 157,
+    'greann' => 158, 
+    'pobal' => 159, 
+    'foghlaimeoiri' => 187,
+    'nuacht' => 191,
+    'other_cat' => 'TODO',
+    'tag' => 'TODO',
+    'cpt' => 'TODO',
+    'page' => 'TODO'
+);
+
+/**
+ * Functions
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * Determine Loop Type
+ * -------------------
+ * I was surprised to find WordPress didn't already have just this.
+ * 
+ * @return  bool    $loop_type      The type of WordPress loop.
+ */
+
+function get_loop_type() {
+    global $wp_query; 
+    $loop_type = 'notfound';
+
+    if ($wp_query->is_page) {
+        $loop_type = is_front_page() ? 'front' : 'page';
+    } elseif ($wp_query->is_home) {
+        $loop_type = 'home';
+    } elseif ($wp_query->is_single) {
+        $loop_type = ($wp_query->is_attachment) ? 'attachment' : 'single';
+    } elseif ($wp_query->is_category) {
+        $loop_type = 'category';
+    } elseif ($wp_query->is_tag) {
+        $loop_type = 'tag';
+    } elseif ($wp_query->is_tax) {
+        $loop_type = 'tax';
+    } elseif ($wp_query->is_archive) {
+        if ($wp_query->is_day) {
+            $loop_type = 'day';
+        } elseif ($wp_query->is_month) {
+            $loop_type = 'month';
+        } elseif ($wp_query->is_year) {
+            $loop_type = 'year';
+        } elseif ($wp_query->is_author) {
+            $loop_type = 'author';
+        } else {
+            $loop_type = 'archive';
         }
-        
-        return $classes;
+    } elseif ($wp_query->is_search) {
+        $loop_type = 'search';
+    } elseif ($wp_query->is_404) {
+        $loop_type = 'notfound';
     }
 
-
-    function __construct($category, $arguments = null) {
-        $this->category = $category;
-        $this->add_filter('body_class', array(&$this, 'add_section_body_class'));
-    } 
+    return $loop_type;
 }
 
-// Pobal: 159
-// Children: 182, 183, 184, 185, 184, 216, 221
+/**
+ * Print Loop Type
+ * ---------------
+ */
 
-// $pobal = new Section(159, array(
-//     'exclude' => array(
-//         'category' => 216,
-//         'category' => 221
-//     ),
-//     'include' => array(
-//         'post' => 159,
-//         'tag' => 100,
-//         'page' => 15
-//         'category' => 33
-//     )
-// ));
+function loop_type() {
+    printf('%s', get_loop_type());
+}
 
-$Pobal = new Section('HELLO MUMMY');
+/**
+ * Test Function, Please Ignore
+ * ----------------------------
+ */
+
+function yolo_swag() {
+    $swag = '';
+
+    switch (get_loop_type()) {
+        case 'home': $swag = 'This is the home page!'; break;
+        case 'single': $swag = 'This is a single post!'; break;
+        case 'category': $swag = get_category_parent_id(get_query_var('cat')); break;
+        default: 'This is the default message, yo!'; break;
+    } 
+
+    return $swag; 
+}
+
+/**
+ * Get ID of Category's Ultimate Parent
+ * ------------------------------------
+ * The site is sectioned into several parent categories with children and 
+ * grandchildren beneath. Recursively ascend through parent categories until you
+ * hit the top.
+ * 
+ * @param   int     $cat_id     
+ * @return  int     $parent         Numeric ID of category's ultimate parent.
+ */
+
+function get_category_parent_id($cat_id) {
+    $category = get_category($cat_id);
+    $parent = $category->category_parent;
+
+    if ($parent) {
+        get_category_parent_id($parent);
+    } 
+
+    return $parent;
+}
+
+/**
+ * Echo ID of Category's Ultiamte Parent
+ * -------------------------------------
+ */
+
+function category_parent_id($cat_id = null) {
+    printf('%s', get_category_parent_id($cat_id));
+}
+
 ?>
