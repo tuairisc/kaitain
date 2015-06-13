571800145599c3dced01a70468bd51bb6e829d6a
Mark <mark@bhalash.com>
2015-06-13 16:56:54 +0100

    Created clean rebuild branch to start from scratch

--- partials/articles/article-featured.php
+++
@@ -1,8 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('featured'); ?>>
-    <header>
-        <div class="tuairisc-featured-thumb" style="background-image: url('<?php echo get_thumbnail_url(get_the_ID(), 'medium'); ?>');">
-            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"> </a>
-        </div>
-        <h5 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h5>
-    </header>
-</article>
