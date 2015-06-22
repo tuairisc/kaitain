e2835f2c2d0564c30291326a85f44d38348ddbe2
Mark Grealish <mark@bhalash.com>
2015-06-22 20:13:49 +0000

    Added article template partials

---
+++ partials/articles/article-full.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Single Article Content
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Tuairisc.ie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2014-2015, Tuairisc Bheo Teo
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    2.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ * @link       http://www.tuairisc.ie
+ *
+ * This file is part of Tuairisc.ie.
+ * 
+ * Tuairisc.ie is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Tuairisc.ie is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Tuairisc.ie. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+
+<article <?php post_class('full'); ?> id="article-<?php the_ID(); ?>">
+    <?php get_template_part(THEME_PARTIALS . 'header', 'full'); ?>
+
+    <main>
+        <?php the_content(__('Read the rest of this post &raquo;', TTD)); ?>
+    </main>
+
+    <?php if (is_single()) {
+        get_template_part(THEME_PARTIALS . '/pagination');
+        get_template_part(THEME_PARTIALS . 'footer', 'full');
+    } ?>
+</article>
---
+++ partials/articles/article-related.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Related Articles
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Tuairisc.ie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2014-2015, Tuairisc Bheo Teo
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    2.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ * @link       http://www.tuairisc.ie
+ *
+ * This file is part of Tuairisc.ie.
+ * 
+ * Tuairisc.ie is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Tuairisc.ie is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Tuairisc.ie. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+
+<article <?php post_class('related'); ?> id="article-<?php the_ID(); ?>">
+    <header>
+        <div>
+            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
+        </div>
+        <h6 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h6>
+
+        <?php if (!is_page()) : ?>
+            <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_date(get_option('date_format')) ?></time></small>
+        <?php endif; ?>
+    </header>
+    <footer>
+        <small><?php edit_post_link('edit post', ' ', ''); ?></small>
+    </footer>
+</article>
