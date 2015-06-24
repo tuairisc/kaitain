78b44f45beb5947e4da3825e1b9ba4139af663bc
Mark <mark@bhalash.com>
2015-06-24 21:37:51 +0100

    Cleaned up code and added boilerplate comments

--- widgets/tuairisc-mostviewed.php
+++ widgets/tuairisc-mostviewed.php
@@ -28,6 +28,11 @@
  */
 
 class tuairisc_popular extends WP_Widget {
+    /**
+     * Widget Constructor
+     * -------------------------------------------------------------------------
+     */
+
     public function __construct() {
         parent::__construct(
             __('tuairisc_popular', TTD),
@@ -37,6 +42,93 @@ class tuairisc_popular extends WP_Widget {
             )
         );
     }
+
+    /**
+     * Widget Administrative Form
+     * -------------------------------------------------------------------------
+     * @param array     $instance       Widget instance.
+     */
+
+    public function form($instance) {
+        $instance = wp_parse_args($instance, $defaults);
+
+        $options = array(
+            // Post duration options.
+            '36500' => 'All Time',
+            '365' => 'This Year',
+            '28' => 'This Month',
+            '14' => 'Two Weeks',
+            '7' => 'Seven Days',
+            '6' => 'Six Days',
+            '5' => 'Five Days',
+            '4' => 'Four Days',
+            '3' => 'Three Days',
+            '2' => 'Two Days',
+            '1' => 'Today',
+        );
+
+        $defaults = array(
+            // Widget defaults.
+            'widget_title' => __('Most Viewed', TTD),
+            'max_posts' => 10,
+            'elapsed_days' => '7'
+        ); ?>
+
+        <script>
+            // This jQuery is easier for me to parse and debug than a mess of inline PHP.
+            jQuery(function($) {
+                // Set 'elapsed_days' selected option.
+                $('<?php printf('#%s', $this->get_field_id("elapsed_days")); ?>').val('<?php printf($instance["elapsed_days"]); ?>');
+                // Set 'max_posts' selected option. 
+                $('<?php printf('#%s', $this->get_field_id("max_posts")); ?>').val('<?php printf($instance["max_posts"]); ?>');
+            });
+        </script>
+        
+        <p>
+            <label for="<?php printf($this->get_field_id('widget_title')); ?>"><?php _e('Title:', TTD); ?></label><br />
+            <input id="<?php printf($this->get_field_id('widget_title')); ?>" name="<?php printf($this->get_field_name('widget_title')); ?>" value="<?php printf($instance['widget_title']); ?>" type="text" class="widefat" />
+        </p>
+        <p>
+            <label for="<?php printf($this->get_field_id('elapsed_days')); ?>"><?php _e('Since:', TTD); ?></label><br />
+            <select id="<?php printf($this->get_field_id('elapsed_days')); ?>" name="<?php printf($this->get_field_name('elapsed_days')); ?>">
+                <?php foreach ($options as $key => $value) {
+                    printf('<option value="%s">%s</option>', $key, $value);
+                }  ?>
+            </select>
+        </p>
+        <p>
+            <label for="<?php printf($this->get_field_id('max_posts')); ?>"><?php _e('Posts To Display:', TTD); ?></label><br />
+            <select id="<?php printf($this->get_field_id('max_posts')); ?>" name="<?php printf($this->get_field_name('max_posts')); ?>">
+                <?php for ($i = 1; $i <= 10; $i++) : ?>
+                    <option value="<?php printf($i); ?>"><?php printf($i); ?></option>
+                <?php endfor; ?>
+            </select>
+        </p>
+        <?php
+    }
+
+    /**
+     * Widget Update
+     * -------------------------------------------------------------------------
+     * @param  array    $new_args       New args variables.
+     * @param  array    $old_args       Old args variables.
+     * @return array    $args           New args settings.
+     */
+
+    function update($new_args, $old_args) {
+        $args = array();
+        $args['widget_title'] = strip_tags($new_args['widget_title']);
+        $args['max_posts'] = $new_args['max_posts'];
+        $args['elapsed_days'] = $new_args['elapsed_days'];
+        return $args;
+    }
+
+    /**
+     * Widget Public Display
+     * -------------------------------------------------------------------------
+     * @param array     $args             Widget Arguments. 
+     * @param 
+     */
 
     public function widget($args, $instance) {
         extract($args);
@@ -97,70 +189,8 @@ class tuairisc_popular extends WP_Widget {
         printf('</ul>');
         printf($after_widget);
     }
-
-    function update($new_instance, $old_instance) {
-        $instance = array();
-        $instance['widget_title'] = strip_tags($new_instance['widget_title']);
-        $instance['max_posts'] = $new_instance['max_posts'];
-        $instance['elapsed_days'] = $new_instance['elapsed_days'];
-        return $instance;
-    }
-
-    public function form($instance) {
-        $instance = wp_parse_args($instance, $defaults);
-
-        $options = array(
-            '36500' => 'All Time',
-            '365' => 'This Year',
-            '28' => 'This Month',
-            '14' => 'Two Weeks',
-            '7' => 'Seven Days',
-            '6' => 'Six Days',
-            '5' => 'Five Days',
-            '4' => 'Four Days',
-            '3' => 'Three Days',
-            '2' => 'Two Days',
-            '1' => 'Today',
-        );
-
-        $defaults = array(
-            'widget_title' => __('Most Viewed', TTD),
-            'max_posts' => 10,
-            'elapsed_days' => '7'
-        ); ?>
-
-        <script>
-            // This jQuery is easier for me to parse and debug than a mess of inline PHP.
-            jQuery(function($) {
-                // Set 'elapsed_days' selected option.
-                $('<?php printf('#%s', $this->get_field_id("elapsed_days")); ?>').val('<?php printf($instance["elapsed_days"]); ?>');
-                // Set 'max_posts' selected option. 
-                $('<?php printf('#%s', $this->get_field_id("max_posts")); ?>').val('<?php printf($instance["max_posts"]); ?>');
-            });
-        </script>
-        <p>
-            <label for="<?php printf($this->get_field_id('widget_title')); ?>"><?php _e('Title:', TTD); ?></label><br />
-            <input id="<?php printf($this->get_field_id('widget_title')); ?>" name="<?php printf($this->get_field_name('widget_title')); ?>" value="<?php printf($instance['widget_title']); ?>" type="text" class="widefat" />
-        </p>
-        <p>
-            <label for="<?php printf($this->get_field_id('elapsed_days')); ?>"><?php _e('Since:', TTD); ?></label><br />
-            <select id="<?php printf($this->get_field_id('elapsed_days')); ?>" name="<?php printf($this->get_field_name('elapsed_days')); ?>">
-                <?php foreach ($options as $key => $value) {
-                    printf('<option value="%s">%s</option>', $key, $value);
-                }  ?>
-            </select>
-        </p>
-        <p>
-            <label for="<?php printf($this->get_field_id('max_posts')); ?>"><?php _e('Posts To Display:', TTD); ?></label><br />
-            <select id="<?php printf($this->get_field_id('max_posts')); ?>" name="<?php printf($this->get_field_name('max_posts')); ?>">
-                <?php for ($i = 1; $i <= 10; $i++) : ?>
-                    <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                <?php endfor; ?>
-            </select>
-        </p>
-        <?php
-    }
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("tuairisc_popular");'));
+
 ?>
