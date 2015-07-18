03000d21e43c40d3bdbf7e3694a86e4e0c15bc9e
Mark Grealish <mark@bhalash.com>
2015-07-18 21:10:42 +0000

    Added job checkbox code

--- admin/job-checkbox.php
+++ admin/job-checkbox.php
@@ -27,4 +27,92 @@
  * Tuairisc.ie. If not, see <http://www.gnu.org/licenses/>.
  */
 
+$job_nonce = array(
+    'action' => 'tuairisc_job_box',
+    'name' => 'tuairisc_job_box_nonce'
+);
+
+/**
+ * Add Post Editor Meta Box
+ * -----------------------------------------------------------------------------
+ */
+
+function tuairisc_job_meta_box() {
+    add_meta_box(
+        'tuairisc_job_meta',
+        __('Job Listing', TTD),
+        'tuairisc_job_box_content',
+        'post'
+    );
+}
+
+/**
+ * Add Post Editor Meta Box
+ * -----------------------------------------------------------------------------
+ * @param   object      $post           Post object.
+ */
+
+function tuairisc_job_box_content($post, $args) {
+    global $job_nonce;
+    wp_nonce_field($job_nonce['action'], $job_nonce['name']);
+
+    $key = get_option('tuairisc_job_post_key');
+    $is_job = !!get_post_meta($post->ID, $key, true) ? 'checked' : '';;
+
+    ?>
+
+    <p>
+        <?php _e('Mark post as a listing for a government or private business employment position.', TTD); ?>
+    </p>
+
+    <ul>
+        <li>
+            <input id="meta-tuairisc-job" name="make-job" type="checkbox" <?php printf($is_job); ?>>
+            <label for="meta-tuairisc-job"><?php _e('Job Listing', TTD); ?></label>
+        </li>
+    </ul>
+
+    <?php
+}
+
+/**
+ * Update Featured Post Meta
+ * -----------------------------------------------------------------------------
+ * @param   int      $post_id           Post object ID.
+ */
+
+function update_job_meta_box($post_id) {
+    global $job_nonce;
+    
+    if (!ctype_alnum($_POST[$job_nonce['name']]) || !isset($_POST[$job_nonce['name']])) {
+        return;
+    }
+
+    if (!wp_verify_nonce($_POST[$job_nonce['name']], $job_nonce['action'])) {
+        return;
+    }
+    
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return; 
+    }
+
+    if (!current_user_can('edit_post', $post_id)) {
+        return;
+    }
+    
+    // UPDATE JOB POST META KEY YOLO MILEY CYRUS
+    $key = get_option('tuairisc_job_post_key');
+    $value = ($_POST['make-job'] === 'on') ? true : false;
+
+    update_post_meta($post_id, $key, $value);
+}
+
+/**
+ * Update Post Meta Box
+ * -----------------------------------------------------------------------------
+ */
+
+add_action('add_meta_boxes', 'tuairisc_job_meta_box');
+add_action('save_post', 'update_job_meta_box');
+
 ?>
