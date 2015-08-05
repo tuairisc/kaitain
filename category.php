d95965b4fd50019249e403633cd9b80dea0b8b54
Mark Grealish <mark@bhalash.com>
2015-08-05 17:34:15 +0000

    Changed category widget post count

--- category.php
+++ category.php
@@ -81,7 +81,7 @@ if ($page_number < 2) {
 
     if (!empty($children_categories)) {
         foreach ($children_categories as $child) {
-            category_widget_output($child->cat_ID);
+            category_widget_output($child->cat_ID, 5);
         }
     }
 }
