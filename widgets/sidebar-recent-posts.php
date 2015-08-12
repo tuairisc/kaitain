4764757769de839570f903b3247a1baf35c6888a
Mark Grealish <mark@bhalash.com>
2015-08-12 16:03:29 +0000

    Cleaned up widget and fixed title output

--- widgets/sidebar-recent-posts.php
+++ widgets/sidebar-recent-posts.php
@@ -154,7 +154,7 @@ class tuairisc_recent extends WP_Widget {
 
         if (!empty($defaults['before_widget'])) {
             printf($defaults['before_widget']);
-            printf($defaults['before_title'] . apply_filters('widget_title', $instance['widget_title']) . $defaults['after_title']);
+            printf($defaults['before_title'] . $title . $defaults['after_title']);
         }
 
         printf('<div class="recent-widget tuairisc-post-widget">');
