892971115fc887cff2211e8bc20706cd28e9a314
Mark Grealish <mark@bhalash.com>
2015-08-30 11:18:01 +0000

    Added partial for missing posts

---
+++ partials/article-missing.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Single Article Content
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Tuairisc.ie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2014-2015, Tuairisc Bheo Teo
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
+ * @version    2.0
+ * @link       https://github.com/bhalash/tuairisc.ie
+ * @link       http://www.tuairisc.ie
+ */
+
+?>
+
+<p><?php _e('No matching posts were found! :&#40;', 'tuairisc'); ?></p>
