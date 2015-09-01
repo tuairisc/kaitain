dfe3c7ce6b1d9afca57ffeeaa28e5fa52d877bbf
Mark Grealish <mark@bhalash.com>
2015-09-01 21:11:36 +0000

    Removed old input and date variables from script

--- assets/js/postmeta-setup.js
+++ assets/js/postmeta-setup.js
@@ -12,73 +12,29 @@
  */
 
 /**
- * Inputs
- * -----------------------------------------------------------------------------
- * Checkbox inputs for featured post and whether to sticky it too.
- * Options for expiry: minute, hour, day, month and year.
- * Classes for the sticky checkbox, and sticky expiry information.
- */
-
-var input = {
-    select: {
-        day: '#expiry-day',
-        month: '#expiry-month',
-        year: '#expiry-year',
-        hour: '#expiry-hour',
-        minute: '#expiry-minute'
-    },
-};
-
-/**
- * Dates
- * -----------------------------------------------------------------------------
- * Current year, month, day and calendar year as Bearla.
- */
-
-// Widget date will be set by WordPress.
-var date = new Date();
-var expiry;
-
-date = {
-    year: date.getFullYear(),
-    month: date.getMonth(),
-    day: date.getDate(),
-    hour: date.getHours(),
-    minute: date.getMinutes(),
-    calendar: [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ],
-    daysInMonth: function(year, month) {
-        // Return days in given month.
-        return new Date(year, month, 0).getDate();
-    }
-};
-
-if (kaitainMetaInfo.sticky) {
-    var expiry = new Date(kaitainMetaInfo.expiry * 1000);
-
-    expiry = {
-        year: expiry.getFullYear(),
-        month: expiry.getMonth(),
-        day: expiry.getDate(),
-        hour: expiry.getHours(),
-        minute: expiry.getMinutes(),
-    };
-} else {
-    expiry = date;
-}
-
-/**
  * Checkbox Setup
  * -----------------------------------------------------------------------------
  * Add change to checkboxes and set state.
  */
 
 jQuery('#kaitain-featured-checkbox').linkedToggle(
-    [], '.kaitain-stickycheck', kaitainMetaInfo.featured
+    [], '.kaitain-stickycheck', pmFeatured.featured
 );
 
 jQuery('#kaitain-sticky-checkbox').linkedToggle(
-    ['#kaitain-featured-checkbox'], '.kaitain-expiryinfo', kaitainMetaInfo.sticky
+    ['#kaitain-featured-checkbox'], '.kaitain-expiryinfo', pmFeatured.sticky
+);
+
+/**
+ * Date and Time Setup
+ * -----------------------------------------------------------------------------
+ * Add change to checkboxes and set state.
+ */
+
+jQuery('#kaitain-sticky-time').checker(
+    'time', 'stickyexpires', pmFeatured.expiry
+);
+
+jQuery('#kaitain-sticky-date').checker(
+    'date', 'stickyexpires', pmFeatured.expiry
 );
