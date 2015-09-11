93d4e00882094f46b113509d4679fcb37ba97563
Mark Grealish <mark@bhalash.com>
2015-09-11 21:22:16 +0000

    Changed language in regular search form to Irish

--- searchform.php
+++ searchform.php
@@ -19,23 +19,22 @@ $query = get_search_query();
 $action = esc_url(home_url('/'));
 $total = $wp_query->found_posts;
 
-$result = 'result';
-$result .= $total > 1 ? 's' : '';
+$result = $total === 1 ? 'torthaí' : 'tordagh';
 
 ?>
 
 <form role="search" class="searchform vspace-half" id="searchform" method="get" action="<?php printf($action); ?>" autocomplete="off">
     <fieldset>
-        <input class="searchform-input" name="s" placeholder="<?php _e('search', 'kaitain'); ?>" type="text" required="required" value="<?php printf($query); ?>">
+        <input class="searchform-input" id="s" name="s" placeholder="<?php _e('curdaigh', 'kaitain'); ?>" type="text" required="required" value="<?php printf($query); ?>">
     </fieldset>
 </form>
 <div class="clearfix search-results-meta">
     <span class="total meta left-float"><?php printf('%d %s', $total, $result); ?></span>
     <span class="total meta right-float">
-        Sort by: 
+        <?php _e('Saghas:', 'sheepie'); ?>
 
-        <a href="<?php arc_search_url('asc'); ?>"><?php _e('oldest', 'kaitain'); ?></a> |
-        <a href="<?php arc_search_url('desc'); ?>"><?php _e('newest', 'kaitain'); ?></a>
+        <a href="<?php arc_search_url('asc'); ?>"><?php _e('sine', 'kaitain'); ?></a> |
+        <a href="<?php arc_search_url('desc'); ?>"><?php _e('is nua', 'kaitain'); ?></a>
     </span>
 </div>
 <hr>
