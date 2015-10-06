0a7a69ed728dba2164b3b11d45e03098a7be544f
Mark Grealish <mark@bhalash.com>
2015-10-06 18:38:46 +0000

    Added function to parse education section banner shortcodes

--- includes/kaitain-education.php
+++ includes/kaitain-education.php
@@ -13,6 +13,14 @@
  * @link       http://www.tuairisc.ie
  */
 
+/**
+ * Education Landing Page Section Panes
+ * -----------------------------------------------------------------------------
+ * Output the HTML for the education section landing page.
+ *
+ * @param   int/string      $category_id_or_slug
+ */
+
 function kaitain_education_section_pane($category_id_or_slug) {
     if (is_numeric($category_id_or_slug)) {
         $category = get_category($category_id_or_slug);
@@ -53,3 +61,42 @@ function kaitain_education_section_pane($category_id_or_slug) {
     printf('</div>'); // Close rightside container.
     printf('</a>'); // Close container.
 }
+
+/**
+ * Education Banner Shortcode
+ * -----------------------------------------------------------------------------
+ * Generate either a tall or short dividing subheading banners for within 
+ * education section posts.
+ * 
+ * @param   array       $attributes         Shortcode attributes.
+ * @param   string      $content            Banner message.
+ * @return  string      $banner             Dividing banner.
+ */
+
+function kaitain_education_banner_shortcode($args, $content = null) {
+    $args = shortcode_atts(array('type' => 'main'), $args);
+
+    // Headline, <h2> or <h3>
+    $headline_type = ($args['type'] === 'main') ?  2 : 3;
+    $headline_class = 'education-banner';
+
+    if (!$content) {
+        $content = __('Did you forget to include text?', 'kaitain');
+    }
+
+    if ($args['type'] !== 'main') {
+        // If the banner is not 'main', change h2 to h2 and heading to subheading.
+        $headline_class .= '-subheading';
+    }
+
+    $banner = sprintf('<h%s class="%s">%s</h%s>', 
+        $headline_type,
+        $headline_class,
+        $content,
+        $headline_type
+    );
+
+    return $banner;
+}
+
+add_shortcode('banner', 'kaitain_education_banner_shortcode');
