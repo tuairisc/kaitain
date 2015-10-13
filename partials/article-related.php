e639009e6da7ffacedbf898c34d7c4610774f18a
Mark Grealish <mark@bhalash.com>
2015-10-13 12:25:44 +0000

    Replaced CSS rules with preset classes

--- partials/article-related.php
+++ partials/article-related.php
@@ -20,12 +20,12 @@ $trim = $sections->section_css_classes(get_the_category()[0]);
 
 <article <?php post_class('related'); ?> id="related-<?php the_ID(); ?>">
     <a class="<?php printf($trim['hover']['text']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-        <div class="thumbnail">
+        <div class="thumbnail vspace--half">
             <?php post_image_html(get_the_ID(), 'tc_post_related', true); ?>
             <div class="archive-trim-bottom <?php printf($trim['reg']['background']); ?>"></div>
         </div>
         <header>
-            <?php the_title('<h5 class="title">', '</h5>'); ?>
+            <?php the_title('<h5 class="title vspace--half">', '</h5>'); ?>
             <?php if (!is_page()) : ?>
                 <span class="post-date"><time datetime="<?php the_date('Y-m-d H:i'); ?>"><?php the_post_date_strftime(); ?></time></span>
             <?php endif; ?>
