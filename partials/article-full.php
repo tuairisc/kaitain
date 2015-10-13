8d8288b9d8ee79d4f20435a389e384c7cfd1cd32
Mark Grealish <mark@bhalash.com>
2015-10-13 12:55:25 +0000

    Cleaned up full article classes

--- partials/article-full.php
+++ partials/article-full.php
@@ -17,23 +17,23 @@ $author = get_the_author_meta('ID');
 $hidden_users = get_option('kaitain_hidden_users');
 
 $post_classes = array(
-    'main-article-full', 'vspace'   
+    'article--full', 'vspace--full'
 );
 
 ?>
 
 <article <?php post_class($post_classes); ?> id="article-<?php the_ID(); ?>">
-    <header>
-        <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <header class="article--full__header">
+        <h1 class="title article--full__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <?php if (!is_page()) : ?>
-            <p class="post-excerpt"><?php printf(get_the_excerpt()); ?></p>
-            <div class="post-meta">
+            <p class="post-excerpt article--full__excerpt"><?php printf(get_the_excerpt()); ?></p>
+            <div class="post-meta article--full__postmeta">
                 <div class="author-meta">
-                    <div class="avatar">
+                    <div class="author-meta__avatar">
                         <?php kaitain_avatar_background_html($author, 'tc_post_avatar', 'author-photo'); ?>
                     </div>
-                    <div class="author-info">
-                        <span class="author-link"><a class="green-link--hover" href="<?php printf(get_author_posts_url($author)); ?>"><?php the_author_meta('display_name'); ?></a></span>
+                    <div class="author-meta__info">
+                        <a class="author-meta__link green-link--hover" href="<?php printf(get_author_posts_url($author)); ?>"><strong><span><?php the_author_meta('display_name'); ?></strong></span></a>
                         <br />
                         <span class="post-date"><time datetime="<?php the_date('Y-m-d H:i'); ?>"><?php the_post_date_strftime(); ?></time></span>
                         <br />
@@ -46,7 +46,7 @@ $post_classes = array(
             </div>
         <?php endif; ?>
     </header>
-    <div class="post-content">
+    <div class="post-content article--full__content">
         <?php the_content(__('Read the rest of this post &raquo;', 'kaitain')); ?>
     </div>
 
