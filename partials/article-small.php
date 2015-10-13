6e60f9e35458ec30e318ad65a08278b3b4248b4d
Mark Grealish <mark@bhalash.com>
2015-10-13 15:35:28 +0000

    Refactored article selectors

--- partials/article-small.php
+++ partials/article-small.php
@@ -16,16 +16,18 @@
 global $sections;
 $trim = $sections->section_css_classes(get_the_category()[0]);
 
+$post_classes = array(
+    'article--small', 'vspace--half'
+);
+
 ?>
 
-<article <?php post_class('archive-small'); ?> id="archive-small-<?php the_id(); ?>">
-    <a class="article-small-link <?php printf($trim['hover']['text']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-        <div class="thumbnail">
+<article <?php post_class($post_classes); ?> id="article--small---<?php the_id(); ?>">
+    <a class="article--small__link <?php printf($trim['hover']['text']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
+        <div class="thumbnail article--small__thumb vspace--half">
             <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
             <div class="archive-trim-bottom <?php printf($trim['reg']['background']); ?>"></div>
         </div>
-        <header>
-            <h5 class="title archive-small-title"><?php the_title(); ?></h5>
-        </header>
+        <h5 class="title article--small__title"><?php the_title(); ?></h5>
     </a>
 </article>
