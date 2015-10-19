adc6337f43911a0e1d829365f4d2aeafd2d2bd83
Mark Grealish <mark@bhalash.com>
2015-10-19 21:13:23 +0000

    Completed menu state script and added commentary

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,29 +13,129 @@
 
 ;(function($, window, document) {
     /**
-     * Knockout Search State Directive
+     * Search and Menu Display Controller
      * -------------------------------------------------------------------------
-     * @param   bool        initialState        Initial state of search.
+     *  This controls the responsive and display states of the site header
+     *  navigation menu and searchform. 
+     *
+     *  @param  bool        initialState        Initial state (show or hide).
      */
 
-    var searchDisplay = function(initialState) {
+    var navViewController = function(initialState) {
         var self = this;
 
-        // Menu state.
-        self.searchOpen = ko.observable(initialState);
-
-        self.toggleSearch = function() {
-            // Toggle search, true/false. Used on menu buttons.
-            self.searchOpen(!self.searchOpen());
+        self.breaks = {
+            // Pixel width breakpoint for scrolling behaviour to end.
+            width: 640
         };
 
-        self.keypressHide = function(data, event) {
-            // Hide search and menu navigations when escape is pressed.
+        self.state = {
+            // Search, menu and menu button states.
+            search: ko.observable(initialState),
+            menu: ko.observable(true),
+            menuButton: ko.observable(false)
+        };
+
+        /**
+         * Toggle Search State
+         * ---------------------------------------------------------------------
+         */
+
+        self.showSearch = function() {
+            // Toggle search, true/false. Used on menu buttons.
+            self.state.search(!self.state.search());
+        };
+
+        /**
+         * Toggle Menu State
+         * ---------------------------------------------------------------------
+         */
+
+        self.showMenu = function() {
+            // Toggle menu, true/false. Used on menu buttons.
+            self.state.menu(!self.state.menu());
+        };
+
+        /**
+         * Hide Menu and Search on Keyup
+         * ---------------------------------------------------------------------
+         *  Hide search and menu navigations when escape is pressed.
+         */
+
+        self.keyupHide = function(data, event) {
             if (event.keyCode === 27) {
-                self.searchOpen(false);
+                self.state.search(false);
+                self.state.menu(false);
             }
         };
+
+        /**
+         * Hide Menu on Window Scroll
+         * ---------------------------------------------------------------------
+         *  This is separate to the conditional scroll below, which includes a 
+         *  toggle on the menu button state.
+         */
+
+        self.onscrollHide = function() {
+            self.state.menu(false);
+        };
+
+        /**
+         * Window Scroll Toggle Control
+         * ---------------------------------------------------------------------
+         */
+
+        self.scrollToggle = function() {
+            // Control the state of the menu and button on scroll.
+
+            var top = $(window).scrollTop();
+            var main = $('#main').offset().top;
+
+            if (!self.state.menu()) {
+                // Add tolerance in order to prevent janky flickering.
+                top += $('#header').height();
+            }
+
+            // If below break, hide menu and show menu button.
+            // Else inverts this behaviour.
+
+            if (top >= main && self.state.menu()) {
+                self.state.menu(false);
+                self.state.menuButton(true);
+            } else if (top < main && !self.state.menu()) {
+                self.state.menu(true);
+                self.state.menuButton(false);
+            }
+        };
+
+        /**
+         *
+         * Window Size Toggle Control
+         * ---------------------------------------------------------------------
+         */
+
+        self.sizeToggle = function() {
+            // If below break, menu button displays and scrolling hevaiour
+            // is set to just hide menu on scroll.
+            // Else inverts this behaviour.
+            //
+            if ($(window).width() <= self.breaks.width) {
+                self.state.menuButton(true);
+                self.state.menu(false);
+
+                $(window).off('scroll', self.scrollToggle);
+                $(window).on('scroll', self.onscrollHide);
+            } else {
+                $(window)
+                    .off('scroll', self.onscrollHide)
+                    .on('scroll', self.scrollToggle)
+                    .trigger('scroll');
+            }
+        };
+
+        // Attach and trigger resize handler.
+        $(window).on('resize', self.sizeToggle).trigger('resize');
     };
 
-    ko.applyBindings(new searchDisplay(false));
+    ko.applyBindings(new navViewController(false));
 })(jQuery, window, document);
