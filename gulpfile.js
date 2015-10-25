503ba4b5d60a45c896dc7c4b3e36175415fe50d1
Mark Grealish <mark@bhalash.com>
2015-10-25 22:35:11 +0000

    Changed build tasks
    * JS will now concatenate into one file.
    * Added Penthouse to optimize CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,34 @@
  * @link       http://www.tuairisc.ie
  */
 
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    prefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    sourcemap = require('gulp-sourcemaps'),
-    replace = require('gulp-replace');
+//
+// Modules
+//
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var prefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemap = require('gulp-sourcemaps');
+var replace = require('gulp-replace');
+var penthouse = require('penthouse');
+var fs = require('fs');
+var concat = require('gulp-concat');
+
+//
+// Assets Paths
+//
 
 var assets = {
     css: {
+        main: 'assets/css/style.css',
+        critical: 'assets/css/critical.css',
         source: 'assets/css/**/*.scss',
         dest: 'assets/css/'
     },
     js: {
+        concat: 'kaitain.js',
         source: 'assets/js/*.js',
         dest: 'assets/js/min/'
     },
@@ -34,6 +48,24 @@ var assets = {
     }
 };
 
+//
+// Penthouse
+//
+
+var phouse = {
+    // DEBUG
+    url: 'https://kaitain.bhalash.com',
+    // url: 'http://www.tuairisc.ie',
+    css: assets.css.main,
+    // Sizes are optimized for mobile delivery, targeting the iPhone 5S.
+    width: 320,
+    height: 568
+};
+
+//
+// Autoprefixer
+//
+
 var prefixes = [
     // Autoprefixer.
     'last 1 version', 
@@ -42,29 +74,59 @@ var prefixes = [
     'ie 9'
 ];
 
+//
+// Regex Replacements
+//
+
 var regex = {
     comments: {
-        // Remove block comments from output CSS.
+        // Remove block comments from unminified output CSS.
         match: /^(\/\*|\s\*|\s{3}=).*[\n\r]/mg,
         replace: ''
     }
 };
+
+// 
+// Optimize Production CSS Layout
+//
+
+gulp.task('penthouse', function() {
+    penthouse(phouse, function(error, css) {
+        if (error) {
+            console.log(error);
+            console.log(css);
+        }
+
+        fs.writeFile(assets.css.main, css);
+    });
+});
+
+//
+// Move Up Sprite Assets
+//
 
 gulp.task('sprites', function() {
     gulp.src(assets.sprites.source)
         .pipe(gulp.dest(assets.sprites.dest));
 });
 
-gulp.task('css', function() {
-    // Production minified sass, without sourcemap.
-    return sass(assets.css.source, {
-            emitCompileError: true,
-            style: 'compressed'
-        })
-        .on('error', sass.logError)
-        .pipe(prefixer(prefixes))
-        .pipe(gulp.dest(assets.css.dest));
+//
+// Production Minified CSS
+//
+
+gulp.task('css', ['penthouse'], function() {
+    sass(assets.css.source, {
+        emitCompileError: true,
+        style: 'compressed'
+    })
+    .on('error', sass.logError)
+    .pipe(prefixer(prefixes))
+    .pipe(gulp.dest(assets.css.dest));
 });
+
+// 
+// Uniminified Test CSS with Sourcemap
+//
 
 gulp.task('css-dev', function() {
     // Development unminified sass, with sourcemap.
@@ -79,20 +141,26 @@ gulp.task('css-dev', function() {
         .pipe(gulp.dest(assets.css.dest));
 });
 
+// 
+// Minify JS Files
+//
+
 gulp.task('js', function() {
     // Minify all scripts in the JS folder.
     return gulp.src(assets.js.source)
         .pipe(uglify())
+        .pipe(concat(assets.js.concat))
         .pipe(gulp.dest(assets.js.dest));
 });
 
+// 
+// Watch Tasks
+//
+
 gulp.task('default', function() {
-    gulp.watch(assets.js.source, ['js']);
     gulp.watch(assets.css.source, ['css']);
-    gulp.watch(assets.sprites.source, ['sprites']);
 });
 
 gulp.task('dev', function() {
     gulp.watch(assets.css.source, ['css-dev']);
-    gulp.watch(assets.sprites.source, ['sprites']);
 });
