958e7c2b046ce153843ff05ae75e26c83fdca83b
Darren Kearney <darrenkearney@users.noreply.github.com>
2016-05-09 17:30:30 +0000

    Homepage HTML

--- front-page.php
+++ front-page.php
@@ -15,6 +15,7 @@
 
 $page_number = intval(get_query_var('paged'));
 get_header('post');
+$placeholder = __('curdaigh', 'kaitain');
 
 /* 1. Big Lead Article.
  * 2. Second and third rows of articles.
@@ -52,13 +53,13 @@ if (!$page_number) {
                     <div class="featured-top-70">
                         <article <?php post_class('article--lead'); ?> id="article--lead--<?php the_id(); ?>">
                             <a class="article--lead__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                <div class="thumbnail article--lead__thumb">
-                                    <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                <div class="thumbnail article--lead__thumb img-frame">
+                                    <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                     <?php post_image_html(get_the_ID(), 'tc_home_feature_lead', true); ?>
                                 </div>
-                                <!--<h1 class="title article--lead__title"><?php the_title(); ?></h1>-->
+                                <h1 class="title article--lead__title"><?php the_title(); ?></h1>
                             </a>
-                           <!--  <header class="article--lead__header vspace--half">
+                       <header class="article--lead__header vspace--half">
                                 <h4 class="article--lead__author">
                                     <?php if (!kaitain_is_verboten_user($author)) : ?>
                                         <a class="article--lead__author-link text--bold green-link--hover" href="<?php printf(get_author_posts_url(get_the_author_meta('ID'))); ?>"><?php the_author(); ?></a>
@@ -66,7 +67,7 @@ if (!$page_number) {
                                 </h4>
                                 <h5 class="post-meta article--lead__meta"><time datetime="<?php echo the_date('Y-m-d H:i'); ?>"><?php the_post_date_strftime(); ?></time></h5>
                             </header> 
-                            <p class="post-excerpt article--lead__excerpt"><?php printf(get_the_excerpt()); ?></p>-->
+                            <p class="post-excerpt article--lead__excerpt"><?php printf(get_the_excerpt()); ?></p>
                         </article>
                         
                     </div>
@@ -75,34 +76,53 @@ if (!$page_number) {
                         <div class="featured-side-100">
                             <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
                                 <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                    <div class="thumbnail article--small__thumb vspace--half">
-                                    <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                    <div class="thumbnail article--small__thumb vspace--half img-frame">
+                                        <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                         <?php post_image_html(get_the_ID(), 'tc_home_feature_small featured-post-image-inner', true); ?>
                                     </div>
                                     <h5 class="title article--small__title"><?php the_title(); ?></h5>
                                 </a>
                             </article>
                         </div>
-                        <div class="featured-top-30-bottom">
+                        <div class="featured-top-30-middle">
                             <div class="featured-side-50 gutter-half-right">
                                 <article class="article--small" id="article--small--<?php the_id(); ?>">
                                     <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                        <div class="thumbnail article--small__thumb">
-                                        <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                        <div class="thumbnail article--small__thumb img-frame vspace--half">
+                                            <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                             <?php post_image_html(get_the_ID(), 'tc_home_feature_small featured-post-image-inner', true); ?>
                                         </div>
-                                        <!-- <h5 class="title article--small__title"><?php the_title(); ?></h5> -->
+                                        <h6 class="title article--small__title"><?php the_title(); ?></h6>
                                     </a>
                                 </article>
                             </div>
                             <div class="featured-side-50 gutter-half-left">
                                 <article class="article--small" id="article--small--<?php the_id(); ?>">
                                     <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                        <div class="thumbnail article--small__thumb">
-                                        <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                        <div class="thumbnail article--small__thumb img-frame vspace--half">
+                                            <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                             <?php post_image_html(get_the_ID(), 'tc_home_feature_small featured-post-image-inner', true); ?>
                                         </div>
-                                        <!-- <h5 class="title article--small__title"><?php the_title(); ?></h5> -->
+                                        <h6 class="title article--small__title"><?php the_title(); ?></h6>
+                                    </a>
+                                </article>
+                            </div>
+                        </div>
+                        <?php
+                        //   _
+                        //  [_]#####
+                        //  The following is the one-third width square thumbnail with 2/3 text to the right
+                        //
+                        ?>
+                        <div class="featured-top-33-container">
+                            <div class="featured-top-33-inner-container">
+                                <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
+                                    <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
+                                        <div class="section-trim <?php printf($trim['bg']); ?>"></div>
+                                            <div class="thumbnail article--small__thumb vspace--half">
+                                            <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
+                                        </div>
+                                        <h6 class="title article--small__title"><?php the_title(); ?></h6>
                                     </a>
                                 </article>
                             </div>
@@ -115,58 +135,14 @@ if (!$page_number) {
                 <?php
                 /* Top article's title and excerpt. Here and not above due to styling requirements*/
                 ?>
+
                 <div class="featured-top-row-2">
-                    <div class="featured-top-70">
-                        <h1 class="title article--lead__title"><?php the_title(); ?></h1>    
-                        <header class="article--lead__header vspace--half">
-                            <h4 class="article--lead__author">
-                                <?php if (!kaitain_is_verboten_user($author)) : ?>
-                                    <a class="article--lead__author-link text--bold green-link--hover" href="<?php printf(get_author_posts_url(get_the_author_meta('ID'))); ?>"><?php the_author(); ?></a>
-                                    <?php endif; ?>
-                            </h4>
-                            <h5 class="post-meta article--lead__meta"><time datetime="<?php echo the_date('Y-m-d H:i'); ?>"><?php the_post_date_strftime(); ?></time></h5>
-                        </header>
-                        <p class="post-excerpt article--lead__excerpt"><?php the_excerpt(); ?></p>
-                        <!-- <hr> -->
-                    </div>
-                    <div class="featured-top-row-2-30">
-                            <article class="featured-side-50 gutter-half-right " style="margin-left: 0; margin-top: 8px;">
-                                <h6 class="title article--small__title"><?php the_title(); ?></h6>
-                            </article>
-
-                            <article class="featured-side-50 gutter-half-left" style="margin-left: 0; margin-top: 8px;">
-                                <h6 class="title article--small__title"><?php the_title(); ?></h6>
-                            </article>
-                    </div>
-                    <?php
-                    //   _
-                    //  [_]#####
-                    //  The following is the one-third width square thumbnail with 2/3 text to the right
-                    //
-                    ?>
-                    <div class="featured-top-33-container">
-                        <div class="featured-top-33-inner-container">
-                            <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
-                                <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                    <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
-                                    <div class="thumbnail article--small__thumb vspace--half">
-                                        <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
-                                    </div>
-                                    <h6 class="title article--small__title"><?php the_title(); ?></h6>
-                                </a>
-                            </article>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="featured-main-area">
                     <?php
                     //
                     // Row
                     //
                     ?>
-                    <div class="featured-row">
+                    <div class="featured-top-70">
 
                         <?php
                         // 50 %
@@ -174,8 +150,8 @@ if (!$page_number) {
                         <div class="featured-50 gutter-half-right">
                             <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
                                 <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
-                                    <div class="thumbnail article--small__thumb vspace--half">
-                                        <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                    <div class="thumbnail article--small__thumb vspace--half img-frame">
+                                        <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                         <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
                                     </div>
                                     <h5 class="title article--small__title"><?php the_title(); ?></h5>
@@ -189,7 +165,7 @@ if (!$page_number) {
                             <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
                                 <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
                                     <div class="thumbnail article--small__thumb vspace--half">
-                                    <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                        <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                         <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
                                     </div>
                                     <h5 class="title article--small__title"><?php the_title(); ?></h5>
@@ -203,12 +179,17 @@ if (!$page_number) {
                             <article class="article--small vspace--half" id="article--small--<?php the_id(); ?>">
                                 <a class="article--small__link <?php printf($trim['texthover']); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
                                     <div class="thumbnail article--small__thumb vspace--half">
-                                    <div class="archive-trim-bottom <?php printf($trim['bg']); ?>"></div>
+                                        <div class="section-trim <?php printf($trim['bg']); ?>"></div>
                                         <?php post_image_html(get_the_ID(), 'tc_home_feature_small', true); ?>
                                     </div>
                                     <h5 class="title article--small__title"><?php the_title(); ?></h5>
                                 </a>
                             </article>
+                        </div>
+                    </div>
+
+                    <div class="featured-top-30">
+                        <div class="featured-top-advert">
                         </div>
                     </div>
                 </div>
